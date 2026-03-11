ff5bcc9655adff030a4ef721e60fd77fb15aa2c9
tonqi <n.thanglong32@gmail.com>
2026-03-11 21:11:25 +0100

    v1.2 added bootstrap to most elements, implemented a button that collapses a window that displays a textarea that sends a post request to views.py function for reply and comment, form gets saved into DB, new_comment/reply therefor not needed, implemented paginator to display a certain number of objects per side, + minor changes

--- threads/forms.py
+++ threads/forms.py
@@ -7,15 +7,24 @@ class ThreadForm(forms.ModelForm):
     class Meta:
         model = Thread
         fields = ['thread_title','thread_description']
-        widgets = {
-            'thread_title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title'}),
-            'thread_description': forms.Textarea(attrs={'class': 'form-control','rows': 3}),
+        labels = {
+            'thread_title': '',
+            'thread_description': '' 
         }
+        widgets = {
+            'thread_title': forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': 'Enter title'}),
+            'thread_description': forms.Textarea(attrs={'class': 'form-control','rows': 3, 'placeholder': "Description"}),
+        }
+
+
 
 class NewCommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['text']
+        labels = {
+            'text': '' 
+        }
         widgets = {
             'text': forms.Textarea(attrs={'class': 'form-control','rows': 3}),
         }
@@ -24,6 +33,9 @@ class NewReplyForm(forms.ModelForm):
     class Meta:
         model = Reply
         fields = ['text']
+        labels = {
+            'text': '' 
+        }
         widgets = {
             'text': forms.Textarea(attrs={'class': 'form-control','rows': 3}),
         }
--- threads/views.py
+++ threads/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from .models import Topic, Thread, Comment
 from .forms import ThreadForm, NewCommentForm, NewReplyForm
 from django.contrib.auth.decorators import login_required
@@ -31,6 +32,10 @@ def topic(request, topic_id):
             topic_message="I fear technology, please no more!"
         case 2:
             topic_message="Don't talk rubbish about my favourite series!"
+        case 3:
+            topic_message="I love anime."
+        case 4:
+            topic_message="Soccer, no it's football!"
         case _:
             topic_message="..."
 
@@ -41,14 +46,20 @@ def threads(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     # Parent -> get specific child with <model>_set
     topic_threads = topic.thread_set.order_by('-date_added')
-    context = {'topics':topic, 'topic_threads':topic_threads}
+    paginator = Paginator(topic_threads, 5)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {'topics':topic, 'topic_threads':page_obj,'page_obj': page_obj}
     return render(request, "threads/threads.html", context)
 
 def in_thread(request, topic_id, thread_id):
     topic = Topic.objects.get(id=topic_id)
     thread = Thread.objects.get(id=thread_id)
     comments = thread.comment_set.order_by('-date_added')
-    context = {'topics':topic, 'threads':thread, 'comments':comments}
+    paginator = Paginator(comments, 5)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {'topics':topic, 'threads':thread, 'comments':page_obj, 'page_obj': page_obj}
     return render(request, "threads/in_thread.html", context)
 
 @login_required
@@ -76,43 +87,33 @@ def new_thread(request, topic_id):
 
 @login_required
 def new_comment(request, topic_id, thread_id):
-    topic = Topic.objects.get(id=topic_id)
     thread = Thread.objects.get(id=thread_id)
     if(thread.closed == False):
-        if request.method != 'POST':
-            form = NewCommentForm()
-        else:
-            form = NewCommentForm(data=request.POST)
-            if form.is_valid():
-                new_thread_comment = form.save(commit=False)
-                new_thread_comment.owner = request.user
-                new_thread_comment.thread = thread
-                new_thread_comment.save()
-                return redirect('threads:in_thread', thread_id=thread_id, topic_id=topic_id)
-        
-        context = {'topic':topic, 'form':form, 'thread':thread}
-        return render(request, 'threads/new_comment.html', context)
+        form = NewCommentForm(data=request.POST)
+        if form.is_valid():
+            new_thread_comment = form.save(commit=False)
+            new_thread_comment.owner = request.user
+            new_thread_comment.thread = thread
+            new_thread_comment.save() 
+            return redirect('threads:in_thread', thread_id=thread_id, topic_id=topic_id)
     else:
+        raise Http404
+    if request.method == 'GET':
         raise Http404
 
 @login_required
 def new_reply(request, topic_id, thread_id, comment_id):
-    topic = Topic.objects.get(id=topic_id)
     thread = Thread.objects.get(id=thread_id)
     comment = Comment.objects.get(id=comment_id)
     if(thread.closed == False):
-        if request.method != 'POST':
-            form = NewReplyForm()
-        else:
-            form = NewReplyForm(data=request.POST)
-            if form.is_valid():
-                new_thread_reply = form.save(commit=False)
-                new_thread_reply.owner = request.user
-                new_thread_reply.comment = comment
-                new_thread_reply.save()
-                return redirect('threads:in_thread', thread_id=thread_id, topic_id=topic_id)
-        
-        context = {'topic':topic, 'form':form, 'thread':thread, 'comment':comment}
-        return render(request, 'threads/new_reply.html', context)
+        form = NewReplyForm(data=request.POST)
+        if form.is_valid():
+            new_thread_reply = form.save(commit=False)
+            new_thread_reply.owner = request.user
+            new_thread_reply.comment = comment
+            new_thread_reply.save()
+            return redirect('threads:in_thread', thread_id=thread_id, topic_id=topic_id)
     else:
+        raise Http404
+    if request.method == 'GET':
         raise Http404
